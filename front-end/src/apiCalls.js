a75d347e9c5fec4d684497a300b7010402e90fd8
lincon-fernandes <lincon3013@gmail.com>
2022-01-17 17:11:04 -0300

    lincon-cocial

--- front-end/src/apiCalls.js
+++ front-end/src/apiCalls.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCredentials, dispath) => {
   dispath({ type: 'LOGIN_START' });
   try {
     const res = await axiosInstance.post('/auth/login', userCredentials);
-    dispath({ type: 'LOGIN_SUCCESS', payload: res });
+    dispath({ type: 'LOGIN_SUCCESS', payload: res.data });
   } catch (err) {
     dispath({ type: 'LOGIN_FAILURE', payload: err });
   }
